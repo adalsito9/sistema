c43d222582b09fff6f48cbd12b88be032c4a1256
adalsito9 <141053531+adalsito9@users.noreply.github.com>
2025-07-22 12:22:52 -0600

    Delete config directory

--- config/APP.php
+++
@@ -1,8 +0,0 @@
-<?php
-    const SERVERURL = "http://localhost/sistema/";
-//    const SERVERURL = "http://192.168.0.254/sistema/";
-    const COMPANY = "SISTEMA INVENTARIO";
-
-    const MONEY = "$";
-
-    date_default_timezone_set("America/Mexico_City");
